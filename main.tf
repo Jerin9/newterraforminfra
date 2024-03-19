023d6fade7a91952b2419a8c575ffc3a23ada8c8
Jerin9 <136110507+Jerin9@users.noreply.github.com>
2024-03-19 12:53:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAU7QF35DGSBOKV75N"
-  secret_key = "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"
+  
 }
 
 resource "aws_instance" "aws-jenkins-ec2" {
